1cf2333671e110a2d3f5b0c882d54e32c9b53313
Oswaldo <od@ytrail.com>
2014-06-16 11:15:37 +0200

    Added methods for simple registration of convertors for simple types

--- odata2-lib/odata-core/src/main/java/org/apache/olingo/odata2/core/edm/CustomTypeConvertorRegistry.java
+++ odata2-lib/odata-core/src/main/java/org/apache/olingo/odata2/core/edm/CustomTypeConvertorRegistry.java
@@ -3,7 +3,9 @@ package org.apache.olingo.odata2.core.edm;
 import java.lang.reflect.Member;
 import java.util.Calendar;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 import org.apache.olingo.odata2.api.edm.EdmSimpleTypeKind;
 
@@ -17,11 +19,18 @@ public abstract class CustomTypeConvertorRegistry {
    * code. For instance, one could use joda-time LocalDateTime instead of Calendar, so a CustomDateTimeTypeConvertor can
    * be registered for this specific objective.
    * 
-   * @param customConvertor
-   * @return A possibly replaced customConvertor previously registered for the same type as the new one
+   * @param convertors
+   * @return A possibly replaced customConvertors previously registered for the same types as the new ones
    */
-  public static CustomTypeConvertor registerCustomConvertor(CustomTypeConvertor customConvertor) {
-    return customConvertors.put(customConvertor.getTypeName(), customConvertor);
+  public static Set<CustomTypeConvertor> register(CustomTypeConvertor... convertors) {
+    Set<CustomTypeConvertor> replaced = new HashSet<CustomTypeConvertor>();
+    for (CustomTypeConvertor customConvertor : convertors) {
+      CustomTypeConvertor previous = customConvertors.put(customConvertor.getTypeName(), customConvertor);
+      if (previous != null) {
+        replaced.add(previous);
+      }
+    }
+    return replaced;
   }
 
   /**
@@ -38,7 +47,11 @@ public abstract class CustomTypeConvertorRegistry {
    * @return
    */
   public static EdmSimpleTypeKind convertToEdmSimpleType(Class<?> type, Member member) {
-    return customConvertors.get(type.getName()).getEdmSimpleType(member);
+    String className = type.getName();
+    if (!customConvertors.containsKey(className)) {
+      throw new IllegalStateException(String.format("Unknown type %s", className));
+    }
+    return customConvertors.get(className).getEdmSimpleType(member);
   }
 
   /**
@@ -65,6 +78,68 @@ public abstract class CustomTypeConvertorRegistry {
    */
   public static Long convertValueToMillis(Object value) {
     return ((CustomDateTimeTypeConvertor) customConvertors.get(value.getClass().getName())).convertValueToMillis(value);
+  }
+
+  // Utility methods for simpler registration of simple types
+
+  private static Set<CustomTypeConvertor> registerSimple(EdmSimpleTypeKind kind, Class<?>... types) {
+    Set<CustomTypeConvertor> replaced = new HashSet<CustomTypeConvertor>();
+    for (Class<?> type : types) {
+      replaced.addAll(register(CustomTypeConvertor.newConvertor(type, kind)));
+    }
+    return replaced;
+  }
+
+  public static Set<CustomTypeConvertor> registerBinary(Class<?>... types) {
+    return registerSimple(EdmSimpleTypeKind.Binary, types);
+  }
+
+  public static Set<CustomTypeConvertor> registerBoolean(Class<?>... types) {
+    return registerSimple(EdmSimpleTypeKind.Boolean, types);
+  }
+
+  public static Set<CustomTypeConvertor> registerByte(Class<?>... types) {
+    return registerSimple(EdmSimpleTypeKind.Byte, types);
+  }
+
+  public static Set<CustomTypeConvertor> registerDecimal(Class<?>... types) {
+    return registerSimple(EdmSimpleTypeKind.Decimal, types);
+  }
+
+  public static Set<CustomTypeConvertor> registerDouble(Class<?>... types) {
+    return registerSimple(EdmSimpleTypeKind.Double, types);
+  }
+
+  public static Set<CustomTypeConvertor> registerGuid(Class<?>... types) {
+    return registerSimple(EdmSimpleTypeKind.Guid, types);
+  }
+
+  public static Set<CustomTypeConvertor> registerInt16(Class<?>... types) {
+    return registerSimple(EdmSimpleTypeKind.Int16, types);
+  }
+
+  public static Set<CustomTypeConvertor> registerInt32(Class<?>... types) {
+    return registerSimple(EdmSimpleTypeKind.Int32, types);
+  }
+
+  public static Set<CustomTypeConvertor> registerInt64(Class<?>... types) {
+    return registerSimple(EdmSimpleTypeKind.Int64, types);
+  }
+
+  public static Set<CustomTypeConvertor> registerSByte(Class<?>... types) {
+    return registerSimple(EdmSimpleTypeKind.SByte, types);
+  }
+
+  public static Set<CustomTypeConvertor> registerSingle(Class<?>... types) {
+    return registerSimple(EdmSimpleTypeKind.Single, types);
+  }
+
+  public static Set<CustomTypeConvertor> registerString(Class<?>... types) {
+    return registerSimple(EdmSimpleTypeKind.String, types);
+  }
+
+  public static Set<CustomTypeConvertor> registerNull(Class<?>... types) {
+    return registerSimple(EdmSimpleTypeKind.Null, types);
   }
 
 }
